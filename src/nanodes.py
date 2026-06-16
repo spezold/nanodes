ff4e4103c4b884b53b1f9eb0afcec1e4050f6303
Simon Pezold <simon.pezold@unibas.ch>
2026-06-16 12:06:48 +0200

    Clarify

--- src/nanodes.py
+++ src/nanodes.py
@@ -147,16 +147,16 @@ class ParallelMapper[S, T](BaseNode):
         locked_source = self._locked_source()
         if self._in_order:
             cont_cls: type[C] = deque
-            pop_next_completed_from: Callable[[C], Future] = lambda cont: cont.popleft()
+            pop_next_from: Callable[[C], Future] = lambda cont: cont.popleft()
             append_to: Callable[[C, T], None] = lambda cont, el: cont.append(el)
         else:
             cont_cls: type[C] = set
-            pop_next_completed_from: Callable[[C], Future] = lambda cont: _pop_next_completed_from(cont)
+            pop_next_from: Callable[[C], Future] = lambda cont: _pop_next_completed_from(cont)
             append_to: Callable[[C, T], None] = lambda cont, el: cont.add(el)
         # Pre-fill with up to `num_workers` tasks
         futures = cont_cls(executor.submit(self._fn, it) for it in islice(locked_source, self._num_workers))
         while futures:  # Yield next result (from oldest task if in-order, from next completed task otherwise), refill
-            yield pop_next_completed_from(futures).result()
+            yield pop_next_from(futures).result()  # `result()` will block without timeout if necessary
             try:
                 append_to(futures, executor.submit(self._fn, next(locked_source)))
             except StopIteration:
